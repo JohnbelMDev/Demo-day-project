513206f797e40215e83a28749c2eaf15716a32db
johnbelmdev <johnbel458@gmail.com>
2021-01-29 11:01:12 -0500

    new changes

--- public/viewScroll.js
+++ public/viewScroll.js
@@ -1,14 +1,19 @@
 // get the nav bar element by the id
         var the_nav = document.getElementById('navbar');
+        var h1 = document.getElementById('h1');
+
         //   function to change the color of the navbar when its on the top, and when its not on the top
         function checkOnScroll() {
             // check if the scroll is at the top, if it is then set the navbar background to white
             if (window.scrollY === 0) {
                 the_nav.setAttribute('style', 'background-color:white !important');
+                h1.setAttribute('style', 'color:black !important');
+
                 // if not at the top, set the navbar background to green
             } else {
                 the_nav.setAttribute('style', 'background-color:#0C2340 !important');
-                the_nav.setAttribute('style', 'color:white !important');
+                the_nav.setAttribute('style', 'background-color:#0C2340 !important');
+                h1.setAttribute('style', 'color:white !important');
 
             }
             console.log(window.scrollY); // log out the Y coordinates of the location of the window scroll
